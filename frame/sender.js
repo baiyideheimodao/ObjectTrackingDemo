74fba879832d803c6b54b80d173f0e7fc7266cfb
白衣的黑魔导 <1850524586@qq.com>
2021-12-29 14:14:56 +0800

    增加录制功能

--- frame/sender.js
+++ frame/sender.js
@@ -21,12 +21,14 @@ let height = screen.height;
 let stringMessage;
 let iceMessage;
 
-// //录像质量
-// let recorderOptions = {
-// 	audioBitsPerSecond : 128000,
-// 	videoBitsPerSecond : 2500000,
-// 	mimeType : 'video/mp4'
-// }
+//录像质量
+let recorderOptions = {
+	audioBitsPerSecond: 128000,
+	videoBitsPerSecond: 2500000,
+	// mimeType : 'video/mp4'
+}
+
+let recorder = new MediaRecorder(canvas.captureStream(25), recorderOptions);
 
 socket.onopen = function () {
 	protobuf.load("awesome.proto").then(function (root) {
@@ -51,7 +53,7 @@ function takepicture(video) {
 			canvas.height = height;
 			context.drawImage(video, 0, 0, width, height);
 			// point1 && 
-			draw(blueprintSet) 
+			draw(blueprintSet)
 			// oval(point1, point2);
 			context.stroke();
 		}
@@ -118,7 +120,7 @@ socket.onmessage = function (event) {
 			point1 = [p[0], p[1]];
 			point2 = [p[2], p[3]];
 			console.log(message);
-			console.log(blueprintSet.size,message.serial,symList);
+			console.log(blueprintSet.size, message.serial, symList);
 			// oval(point1, point2);
 			if (blueprintSet.size < message.serial) {
 				symList[message.serial] = Symbol(message.shape);
@@ -137,3 +139,29 @@ socket.onmessage = function (event) {
 // var PeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
 // var SessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
 
+let record = document.getElementsByTagName('button');
+let chunks = [];
+record[0].onclick = function () {
+	recorder.start();
+	console.log(recorder.state);
+}
+record[1].onclick = function () {
+	console.log(recorder);
+	let blob = recorder.stop();
+	console.log(blob);
+}
+
+recorder.ondataavailable = function (e) {
+	console.log(e);
+	chunks.push(e.data);
+	console.log(chunks);
+	let file = new File(chunks, './1.mkv', {
+		type: e.data.type
+	});
+	const link = document.getElementById('download');
+	const downloadUrl = window.URL.createObjectURL(e.data);
+	link.href = downloadUrl;
+	link.download = `1.mkv`;
+	// document.body.appendChild(link);
+	console.log(file);
+}
